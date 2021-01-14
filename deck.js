7ccbeebdb87206112d48622ce45a8bd5b38d4e64
Connor Anderson Larson <Connorandersonlarson@gmail.com>
2021-01-14 15:19:00 -0600

    add game functions to js, improve CSS

---
+++ deck.js
@@ -0,0 +1,210 @@
+var deck = [
+  var spade1 = {
+    name: 'ace',
+    image: <img src="assets/blue-01.png" class="card" alt="Ace of Spades">
+  },
+  var spade2 = {
+    name: 'two',
+    image: <img src="assets/blue-02.png" class="card" alt="Two of Spades">
+  },
+  var spade3 = {
+    name: 'three',
+    image: <img src="assets/blue-03.png" class="card" alt="Three of Spades">
+  },
+  var spade4 = {
+    name: 'four',
+    image: <img src="assets/blue-04.png" class="card" alt="Four of Spades">
+  },
+  var spade5 = {
+    name: 'five',
+    image: <img src="assets/blue-05.png" class="card" alt="Five of Spades">
+  },
+  var spade6 = {
+    name: 'six',
+    image: <img src="assets/blue-06.png" class="card" alt="Six of Spades">
+  },
+  var spade7 = {
+    name: 'seven',
+    image: <img src="assets/blue-07.png" class="card" alt="Seven of Spades">
+  },
+  var spade8 = {
+    name: 'eight',
+    image: <img src="assets/blue-08.png" class="card" alt="Eight of Spades">
+  },
+  var spade9 = {
+    name: 'nine',
+    image: <img src="assets/blue-09.png" class="card" alt="Nine of Spades">
+  },
+  var spade10 = {
+    name: 'ten',
+    image: <img src="assets/blue-10.png" class="card" alt="Ten of Spades">
+  },
+  var spade11 = {
+    name: 'jack',
+    image: <img src="assets/blue-jack.png" class="card" alt="Jack of Spades">
+  },
+  var spade12 = {
+    name: 'queen',
+    image: <img src="assets/blue-queen.png" class="card" alt="Queen of Spades">
+  },
+  var spade13 = {
+    name: 'king',
+    image: <img src="assets/blue-king.png" class="card" alt="King of Spades">
+  },
+  var diamond1 = {
+    name: 'ace',
+    image: <img src="assets/gold-01.png" class="card" alt="Ace of Diamonds">
+  },
+  var diamond2 = {
+    name: 'two',
+    image: <img src="assets/gold-02.png" class="card" alt="Two of Diamonds">
+  },
+  var diamond3 = {
+    name: 'three',
+    image: <img src="assets/gold-03.png" class="card" alt="Three of Diamonds">
+  },
+  var diamond4 = {
+    name: 'four',
+    image: <img src="assets/gold-04.png" class="card" alt="Four of Diamonds">
+  },
+  var diamond5 = {
+    name: 'five',
+    image: <img src="assets/gold-05.png" class="card" alt="Five of Diamonds">
+  },
+  var diamond6 = {
+    name: 'six',
+    image: <img src="assets/gold-06.png" class="card" alt="Six of Diamonds">
+  },
+  var diamond7 = {
+    name: 'seven',
+    image: <img src="assets/gold-07.png" class="card" alt="Seven of Diamonds">
+  },
+  var diamond8 = {
+    name: 'eight',
+    image: <img src="assets/gold-08.png" class="card" alt="Eight of Diamonds">
+  },
+  var diamond9 = {
+    name: 'nine',
+    image: <img src="assets/gold-09.png" class="card" alt="Nine of Diamonds">
+  },
+  var diamond10 = {
+    name: 'ten',
+    image: <img src="assets/gold-10.png" class="card" alt="Ten of Diamonds">
+  },
+  var diamond11 = {
+    name: 'jack',
+    image: <img src="assets/gold-jack.png" class="card" alt="Jack of Diamonds">
+  },
+  var diamond12 = {
+    name: 'queen',
+    image: <img src="assets/gold-queen.png" class="card" alt="Queen of Diamonds">
+  },
+  var diamond13 = {
+    name: 'king',
+    image: <img src="assets/gold-king.png" class="card" alt="King of Diamonds">
+  },
+  var club1 = {
+    name: 'ace',
+    image: <img src="assets/green-01.png" class="card" alt="Ace of Clubs">
+  },
+  var club2 = {
+    name: 'two',
+    image: <img src="assets/green-02.png" class="card" alt="Two of Clubs">
+  },
+  var club3 = {
+    name: 'three',
+    image: <img src="assets/green-03.png" class="card" alt="Three of Clubs">
+  },
+  var club4 = {
+    name: 'four',
+    image: <img src="assets/green-04.png" class="card" alt="Four of Clubs">
+  },
+  var club5 = {
+    name: 'five',
+    image: <img src="assets/green-05.png" class="card" alt="Five of Clubs">
+  },
+  var club6 = {
+    name: 'six',
+    image: <img src="assets/green-06.png" class="card" alt="Six of Clubs">
+  },
+  var club7 = {
+    name: 'seven',
+    image: <img src="assets/green-07.png" class="card" alt="Seven of Clubs">
+  },
+  var club8 = {
+    name: 'eight',
+    image: <img src="assets/green-08.png" class="card" alt="Eight of Clubs">
+  },
+  var club9 = {
+    name: 'nine',
+    image: <img src="assets/green-09.png" class="card" alt="Nine of Clubs">
+  },
+  var club10 = {
+    name: 'ten',
+    image: <img src="assets/green-10.png" class="card" alt="Ten of Clubs">
+  },
+  var club11 = {
+    name: 'jack',
+    image: <img src="assets/green-jack.png" class="card" alt="Jack of Clubs">
+  },
+  var club12 = {
+    name: 'queen',
+    image: <img src="assets/green-queen.png" class="card" alt="Queen of Clubs">
+  },
+  var club13 = {
+    name: 'king',
+    image: <img src="assets/green-king.png" class="card" alt="King of Clubs">
+  },
+  var heart1 = {
+    name: 'ace',
+    image: <img src="assets/red-01.png" class="card" alt="Ace of Hearts">
+  },
+  var heart2 = {
+    name: 'two',
+    image: <img src="assets/red-02.png" class="card" alt="Two of Hearts">
+  },
+  var heart3 = {
+    name: 'three',
+    image: <img src="assets/red-03.png" class="card" alt="Three of Hearts">
+  },
+  var heart4 = {
+    name: 'four',
+    image: <img src="assets/red-04.png" class="card" alt="Four of Hearts">
+  },
+  var heart5 = {
+    name: 'five',
+    image: <img src="assets/red-05.png" class="card" alt="Five of Hearts">
+  },
+  var heart6 = {
+    name: 'six',
+    image: <img src="assets/red-06.png" class="card" alt="Six of Hearts">
+  },
+  var heart7 = {
+    name: 'seven',
+    image: <img src="assets/red-07.png" class="card" alt="Seven of Hearts">
+  },
+  var heart8 = {
+    name: 'eight',
+    image: <img src="assets/red-08.png" class="card" alt="Eight of Hearts">
+  },
+  var heart9 = {
+    name: 'nine',
+    image: <img src="assets/red-09.png" class="card" alt="Nine of Hearts">
+  },
+  var heart10 = {
+    name: 'ten',
+    image: <img src="assets/red-10.png" class="card" alt="Ten of Hearts">
+  },
+  var heart11 = {
+    name: 'jack',
+    image: <img src="assets/red-jack.png" class="card" alt="Jack of Hearts">
+  },
+  var heart12 = {
+    name: 'queen',
+    image: <img src="assets/red-queen.png" class="card" alt="Queen of Hearts">
+  },
+  var heart13 = {
+    name: 'king',
+    image:  <img src="assets/red-king.png" class="card" alt="King of Hearts">
+  },
+]
--- game.js
+++ game.js
@@ -1,3 +1,43 @@
 class Game {
+  constructor(player1, player2) {
+    this.playerOne = player1;
+    this.playerTwo = player2;
+    this.newDeck = [];
+    this.playedCards = [];
+    this.Winner = '';
+    this.slapResult = '';
+  }
+
+  playerTurn(player) {
+    this.playedCards.unshift(player.playCard())
+  }
+
+  slapCard(player) {
+    if (this.playedCards[0].name === 'jack') {
+      this.slapJack(player);
+    } else if (this.playedCards[0] === this.playedCards[1]) {
+      this.doubles(player);
+    } else if (this.playedCards[0] === this.playedCards[2]) {
+      this.sandwich(player);
+    } else {
+
+    }
+  }
+
+  slapJack(player) {
+    this.slapResult = '';
+    for (let i = 0; i < this.playedCards.length; i++) {
+      [player].hand.push(this.playedCards[i]);
+    }
+    this.playedCards = [];
+  }
+
+  doubles(player) {
+    this.slapResult = '';
+  }
+
+  sandwich(player) {
+    this.slapResult = '';
+  }
 
 }
